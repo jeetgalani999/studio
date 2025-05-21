cc18c0c5b39e922ebdfbde3fd9bab4c5a4c4751a
Jeet galani <jeetgalani999@gmail.com>
2025-05-21 11:19:11 +0000

    Resume link :- https://drive.google.com/file/d/1LPnAHsG1WHUxjRC9LeYBJzfSX0_G17BI/view?usp=sharing

--- src/components/ResumeDownloadButton.tsx
+++ src/components/ResumeDownloadButton.tsx
@@ -4,9 +4,9 @@ import { Download } from 'lucide-react';
 export function ResumeDownloadButton(props: ButtonProps) {
   return (
     <Button asChild {...props}>
-      <a href="/resume.pdf" download="YourName_Resume.pdf">
+      <a href="https://drive.google.com/file/d/1LPnAHsG1WHUxjRC9LeYBJzfSX0_G17BI/view?usp=sharing" target="_blank" rel="noopener noreferrer">
         <Download className="mr-2 h-4 w-4" />
-        Download Resume
+        View Resume
       </a>
     </Button>
   );
